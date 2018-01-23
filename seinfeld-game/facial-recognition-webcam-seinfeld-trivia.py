b79693bb0cfe4e112da6631b3003cc599b8be163
NFabrizio <nick.fabrizio@pearson.com>
2018-01-23 08:57:37 -0700

    seinfeld-game Adds more trivia questions

--- seinfeld-game/facial-recognition-webcam-seinfeld-trivia.py
+++ seinfeld-game/facial-recognition-webcam-seinfeld-trivia.py
@@ -13,7 +13,12 @@ known_people_folder = sys.argv[1]
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
-answer_options = ["Elaine Benes", "George Costanza", "Jerry Seinfeld", "Kosmo Kramer"]
+answer_options = [
+"Cosmo Kramer",
+"Elaine Benes",
+"George Costanza",
+"Jerry Seinfeld"
+]
 
 # Add some game functions
 def add_point():
@@ -90,10 +95,22 @@ known_names, known_face_encodings = face_recognition_cli.scan_known_people(known
 process_this_frame = True
 score = 0
 trivia = [
-["Who is the star of Seinfeld?", "Jerry Seinfeld"],
-["Who plays Jerry's neighbor?", "Kosmo Kramer"],
+["Who is Jerry's neighbor?", "Cosmo Kramer"],
+["Who grew up in a household where they had to be in bed by 9pm?", "Cosmo Kramer"],
+["Whose father never made an appearance on the show?", "Cosmo Kramer"],
+["Who never completed high school?", "Cosmo Kramer"],
+["Who has a sister named Gail?", "Elaine Benes"],
+["Who is the worst dancer?", "Elaine Benes"],
+["Whose mother never made an appearance on the show?", "Elaine Benes"],
+["Who is a Baltimore Orioles fan?", "Elaine Benes"],
 ["Who worked at the New York Yankees?", "George Costanza"],
-["Who is the worst dancer?", "Elaine Benes"]
+["Who starts the master of their domain contest?", "George Costanza"],
+["Who cheated on their IQ test?", "George Costanza"],
+["Who gets fired from Pendant Publishing?", "George Costanza"],
+["Who is the star of Seinfeld?", "Jerry Seinfeld"],
+["Who gets sick after eating a black and white cookie?", "Jerry Seinfeld"],
+["Who dated Bette Middler's understudy?", "Jerry Seinfeld"],
+["Who broke up with the person they were dating because of a commercial?", "Jerry Seinfeld"]
 ]
 trivia_length = 0
 
