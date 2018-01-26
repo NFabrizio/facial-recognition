cea5965d3a7979d7bd6e2cc882792de03ef2326c
NFabrizio <nick.fabrizio@pearson.com>
2018-01-26 09:08:03 -0700

    facial-recognition-game-framework Adds initial files and examples

---
+++ facial-recognition-trivia-game-framework/examples/config.py
@@ -0,0 +1,28 @@
+answer_options = [
+"Cosmo Kramer",
+"Elaine Benes",
+"George Costanza",
+"Jerry Seinfeld"
+]
+
+trivia = [
+["Who is Jerry's neighbor?", "Cosmo Kramer"],
+["Who grew up in a household where they had to be in bed by 9pm?", "Cosmo Kramer"],
+["Whose father never made an appearance on the show?", "Cosmo Kramer"],
+["Who never completed high school?", "Cosmo Kramer"],
+["Who has a sister named Gail?", "Elaine Benes"],
+["Who is the worst dancer?", "Elaine Benes"],
+["Whose mother never made an appearance on the show?", "Elaine Benes"],
+["Who is a Baltimore Orioles fan?", "Elaine Benes"],
+["Who worked at the New York Yankees?", "George Costanza"],
+["Who starts the master of their domain contest?", "George Costanza"],
+["Who cheated on their IQ test?", "George Costanza"],
+["Who gets fired from Pendant Publishing?", "George Costanza"],
+["Who is the star of Seinfeld?", "Jerry Seinfeld"],
+["Who gets sick after eating a black and white cookie?", "Jerry Seinfeld"],
+["Who dated Bette Middler's understudy?", "Jerry Seinfeld"],
+["Who broke up with the person they were dating because of a commercial?", "Jerry Seinfeld"]
+]
+
+loser_phrase = "Sorry, sucker! You lost!"
+winner_phrase = "Good on ya. You are a winner! You really know your stuff!"
